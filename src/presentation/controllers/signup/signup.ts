171f9471b43abc37a935b1283e07c46eb885fd0f
Gustavo Santos <gustavosisa@outlook.com>
2021-03-17 14:11:30 -0300

    feature: email validation

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,10 +1,17 @@
+import { InvalidParamError } from '../../errors/invalid-param-error'
 import { MissingParamError } from '../../errors/missing-param-error'
 import { BadRequest } from '../../helper/badRequest'
 import { Controller } from '../../protocols/controller'
+import { EmailValidator } from '../../protocols/email-validator'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 import { SignupDTO } from './signup.dto'
 
 export class SignupController implements Controller {
+  private readonly emailValidator: EmailValidator
+  constructor (emailValidator: EmailValidator) {
+    this.emailValidator = emailValidator
+  }
+
   handle (httpRequest: HttpRequest): HttpResponse {
     const requiredFields: (keyof SignupDTO)[] = ['name', 'email', 'password', 'passwordConfirmation']
 
@@ -15,6 +22,12 @@ export class SignupController implements Controller {
     }
     if (httpRequest.body.password !== httpRequest.body.passwordConfirmation) {
       return BadRequest(new Error('passwords dont match'))
+    }
+
+    const { email } = httpRequest.body as SignupDTO
+
+    if (!this.emailValidator.isValid(email)) {
+      return BadRequest(new InvalidParamError('email'))
     }
   }
 }
