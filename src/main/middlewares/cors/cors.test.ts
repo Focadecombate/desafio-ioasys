34fba475faa0fc2bef57b208ad78334c8cb540b2
Gustavo Santos <gustavosisa@outlook.com>
2021-03-17 21:42:11 -0300

    fix: adds test name to Cors Middleware test

--- src/main/middlewares/cors/cors.test.ts
+++ src/main/middlewares/cors/cors.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest'
 import app from '../../config/app'
 describe('Cors Middleware', () => {
-  test('should ', async () => {
+  test('should have cors headers', async () => {
     app.post('/test_cors', (req, res) => {
       res.send()
     })
