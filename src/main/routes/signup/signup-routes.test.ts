aa53055f38c40109681d5a848fb146e321d83c74
Gustavo Santos <gustavosisa@outlook.com>
2021-03-17 22:27:17 -0300

    refactor: adds routine to connect e clear database before integration tests

--- src/main/routes/signup/signup-routes.test.ts
+++ src/main/routes/signup/signup-routes.test.ts
@@ -1,6 +1,22 @@
 import request from 'supertest'
+import { prismaHelper } from '../../../infra/database/prisma/utils/prisma-client'
 import app from '../../config/app'
 describe('Signup Routes', () => {
+  beforeAll(async () => {
+    await prismaHelper.connect()
+  })
+  beforeEach(async () => {
+    prismaHelper.prismaClient.user.deleteMany({
+      where: {
+        email: {
+          contains: 'any'
+        }
+      }
+    })
+  })
+  afterAll(async () => {
+    await prismaHelper.prismaClient.$disconnect()
+  })
   test('should return an account on success', async () => {
     await request(app)
       .post('/api/signup')
