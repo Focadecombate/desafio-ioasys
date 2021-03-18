aaf3ad5c6514e48baf277c626b90814cb5ddeec0
Gustavo Santos <gustavosisa@outlook.com>
2021-03-18 17:12:15 -0300

    test: ensure if sign throws jwt adapter throws

--- src/infra/cryptography/jwt/jwtAdapter.spec.ts
+++ src/infra/cryptography/jwt/jwtAdapter.spec.ts
@@ -6,11 +6,31 @@ const makeSut = () => {
   return ({ sut })
 }
 
+jest.mock('jsonwebtoken', () => ({
+  async sign (): Promise<string> {
+    return new Promise((resolve) => resolve('any_id'))
+  }
+}))
+
 describe('Jwt Adapter', () => {
   test('should call generate with the correct values', async () => {
     const { sut } = makeSut()
     const signSpy = jest.spyOn(jwt, 'sign')
     await sut.encrypt('any_id')
     expect(signSpy).toHaveBeenCalledWith({ id: 'any_id' }, 'secretKey')
+  })
+  test('should return a token in success', async () => {
+    const { sut } = makeSut()
+    const accessToken = await sut.encrypt('any_id')
+    expect(accessToken).toBe('any_id')
+  })
+  test('compare should pass the exception to caller', async () => {
+    const { sut } = makeSut()
+    jest.spyOn(jwt, 'sign')
+      .mockImplementationOnce(() => new Promise((resolve, reject) => {
+        reject(new Error())
+      }))
+    const tokenGeneratePromise = sut.encrypt('any_value')
+    expect(tokenGeneratePromise).rejects.toThrow()
   })
 })
