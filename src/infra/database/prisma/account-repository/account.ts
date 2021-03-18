3e08d25a3cecc1c40be9b2857f3af3c7fd2cf030
Gustavo Santos <gustavosisa@outlook.com>
2021-03-18 17:25:28 -0300

    feature: ensure findbyEmail return a account on success

--- src/infra/database/prisma/account-repository/account.ts
+++ src/infra/database/prisma/account-repository/account.ts
@@ -1,9 +1,10 @@
 import { AddAccountRepository } from '../../../../data/protocols/db/add-account-repository'
+import { LoadAccountByEmailRepository } from '../../../../data/protocols/db/load-account-by-email-repository'
 import { AccountModel } from '../../../../domain/models/account'
 import { AddAccountModel } from '../../../../domain/usecases/add-account'
 import { prisma } from '../utils/prisma-client'
 
-export class AccountPrismaRepository implements AddAccountRepository {
+export class AccountPrismaRepository implements AddAccountRepository, LoadAccountByEmailRepository {
   async add (account: AddAccountModel): Promise<AccountModel> {
     const { email, name, password } = account
     const addedAccount = await prisma.user.create({
@@ -13,6 +14,15 @@ export class AccountPrismaRepository implements AddAccountRepository {
         password
       }
     })
-    return new Promise(resolve => resolve(addedAccount))
+    return addedAccount
+  }
+
+  async loadByEmail (email: string): Promise<AccountModel> {
+    const account = await prisma.user.findFirst({
+      where: {
+        email
+      }
+    })
+    return account
   }
 }
