6aaebd1f75213a6412a9f2f8d4d2556cdb031017
Gustavo Santos <gustavosisa@outlook.com>
2021-03-18 20:56:59 -0300

    test: ensure add movie controller calls AddMovie with correct value

--- src/presentation/controllers/movie/add-movie/add-movie.spec.ts
+++ src/presentation/controllers/movie/add-movie/add-movie.spec.ts
@@ -1,6 +1,6 @@
-import { Validation } from '../../../protocols'
+import { HttpRequest, AddMovie, AddMovieModel, Validation } from './add-movie-controller-protocols'
+import { badRequest } from '../../login/signup/signup-protocols'
 import { AddMovieController } from './add-movie'
-import { HttpRequest } from './add-movie-controller-protocols'
 
 const makeFakeRequest = (): HttpRequest => ({
   body: {
@@ -10,7 +10,7 @@ const makeFakeRequest = (): HttpRequest => ({
     authorName: 'any_author'
   }
 })
-const makeValidatorStub = () => {
+const makeValidatorStub = (): Validation => {
   class ValidationStub implements Validation {
     validate (input: any): Error {
       return null
@@ -18,11 +18,21 @@ const makeValidatorStub = () => {
   }
   return new ValidationStub()
 }
+const makeAddMovieStub = (): AddMovie => {
+  class AddMovieStub implements AddMovie {
+    async add (data: AddMovieModel): Promise<void> {
+      return new Promise(resolve => resolve())
+    }
+  }
+  return new AddMovieStub()
+}
 const makeSut = () => {
   const validationStub = makeValidatorStub()
+  const addMovieStub = makeAddMovieStub()
   return {
-    sut: new AddMovieController(validationStub),
-    validationStub
+    sut: new AddMovieController(validationStub, addMovieStub),
+    validationStub,
+    addMovieStub
   }
 }
 describe('Add Movie Controller', () => {
@@ -31,5 +41,18 @@ describe('Add Movie Controller', () => {
     const validateSpy = jest.spyOn(validationStub, 'validate')
     await sut.handle(makeFakeRequest())
     expect(validateSpy).toHaveBeenCalledWith(makeFakeRequest().body)
+  })
+  test('should return 400 if validations fails', async () => {
+    const { sut, validationStub } = makeSut()
+    jest.spyOn(validationStub, 'validate')
+      .mockReturnValueOnce(new Error())
+    const httpResponse = await sut.handle(makeFakeRequest())
+    expect(httpResponse).toEqual(badRequest(new Error()))
+  })
+  test('should call addMovie with correct values', async () => {
+    const { sut, addMovieStub } = makeSut()
+    const addSpy = jest.spyOn(addMovieStub, 'add')
+    await sut.handle(makeFakeRequest())
+    expect(addSpy).toHaveBeenCalledWith(makeFakeRequest().body)
   })
 })
