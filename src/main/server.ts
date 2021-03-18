4f39cfd7e0a568f74a24c6402af694665bf986a0
Gustavo Santos <gustavosisa@outlook.com>
2021-03-17 22:26:37 -0300

    chore: setup inicialization to first connect to db than create the app instance

--- src/main/server.ts
+++ src/main/server.ts
@@ -1,4 +1,9 @@
-import app from './config/app'
-
+import { prisma } from '../infra/database/prisma/utils/prisma-client'
 const port = 3001
-app.listen(port, () => console.log(`Server running at http://localhost/${port}`))
+
+prisma.$connect()
+  .then(async () => {
+    const app = (await import('./config/app')).default
+    app.listen(port, () => console.log(`Server running at http://localhost/${port}`))
+  })
+  .catch(console.log)
