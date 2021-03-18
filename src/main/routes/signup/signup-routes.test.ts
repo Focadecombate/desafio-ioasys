a9df675c634f0ce5b46c2860129f770986690bf9
Gustavo Santos <gustavosisa@outlook.com>
2021-03-17 22:10:53 -0300

    test: ensure singnup-route exists

---
+++ src/main/routes/signup/signup-routes.test.ts
@@ -0,0 +1,15 @@
+import request from 'supertest'
+import app from '../../config/app'
+describe('Signup Routes', () => {
+  test('should return an account on success', async () => {
+    await request(app)
+      .post('/api/signup')
+      .send({
+        name: 'valid_name',
+        email: 'valid_email@email.com',
+        password: 'valid_password',
+        passwordConfirmation: 'valid_password'
+      })
+      .expect(200)
+  })
+})
